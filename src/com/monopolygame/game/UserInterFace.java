53684002f10c03afc312df70d91613c33fdccd9a
Daniel Persson <persson.daniel.1990@gmail.com>
2021-02-02 00:35:45 +0100

    Begin add methods to UserInterface

--- src/com/monopolygame/game/UserInterFace.java
+++ src/com/monopolygame/game/UserInterFace.java
@@ -1,5 +1,24 @@
 package com.monopolygame.game;
 
-public class UserInterFace {
+import java.util.Scanner;
 
+public class UserInterFace {
+    public int getNumberOfPlayers() {
+        System.out.println("Please enter the amount of players: ");
+        Scanner sc = new Scanner(System.in);
+
+        int numberOfPlayers = sc.nextInt();
+        sc.close();
+
+        return numberOfPlayers;
+    }
+
+    private String createPlayerName() {
+        System.out.println("Please enter the player name: ");
+        Scanner sc = new Scanner(System.in);
+
+        String playerName = sc.nextLine();
+
+        return playerName;
+    }
 }
