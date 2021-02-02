5ab0ecc326dc400d62120604f03b4dd438abc337
Daniel Persson <persson.daniel.1990@gmail.com>
2021-02-03 00:47:37 +0100

    Add more methods to user interface

--- src/com/monopolygame/game/UserInterFace.java
+++ src/com/monopolygame/game/UserInterFace.java
@@ -14,7 +14,7 @@ public class UserInterFace {
     }
 
     public String createPlayerName() {
-        System.out.println("Please enter the player name: ");
+        System.out.println("Please enter the player or computer name: ");
         Scanner sc = new Scanner(System.in);
 
         // Validate name input here later.
@@ -23,12 +23,25 @@ public class UserInterFace {
         return playerName;
     }
 
-    public void playerMoves(String name, int numberOfSteps) {
-        System.out.println(name + " moves");
+    public void playerMoves(String playerName, int numberOfSteps) {
+        String output = String.format("%s moves a total of %s steps.", playerName, numberOfSteps);
+        System.out.println(output);
     }
 
     public void displayMaxRoundsVictory(String playerName, int credits) {
 
+    }
+
+    public boolean isHuman() {
+        System.out.println("If the next player is a human press 1 other press 2: ");
+        Scanner sc = new Scanner(System.in);
+
+        int pickedNumber = 0;
+        while(pickedNumber != 1 && pickedNumber != 2) {
+            pickedNumber = sc.nextInt();
+        }
+
+        return pickedNumber == 1;
     }
 
     public void displayCreditChange(boolean creditsAdded, int amount, String playerName) {
@@ -47,6 +60,7 @@ public class UserInterFace {
 
     public void buyProperty(int amount, String playerName, String streetName) {
         String output = String.format("%s bought %s for a total of %s credits", playerName, streetName, amount);
+        System.out.println(output);
     }
 
     public void displayBankrubtVictory(String winningPlayerName, String losingPlayerName, int credits) {
